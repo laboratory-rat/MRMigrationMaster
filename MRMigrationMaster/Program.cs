0010f0e9aa7ce2721fa86a1335435d6e171cb6d1
oleg.timofeev <oleg.timofeev20@gmail.com>
2019-04-04 21:41:40 +0300

    Add project files.

---
+++ MRMigrationMaster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MRMigrationMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
